5d5a545e5e9459f237cfe44e9176b40e9c8f1ca1
Ryan Di <git@aceougi.com>
2016-07-03 12:33:46 +0200

    Fix command name

--- bin/server.php
+++ bin/server.php
@@ -1,10 +1,10 @@
 <?php require __DIR__.'/../../../autoload.php';
 
-$cmd = 'ping riot.de';
+$cmd = 'ping riot.de'; // php -S 127.0.0.1:8080 router.php
 
 chdir(__DIR__.'/../../../../');
 
-$cmd = $_GET['cmd'];
+//$cmd = $_GET['cmd'];
 
 $pipes = [];
 
