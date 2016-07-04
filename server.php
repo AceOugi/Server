25f8759f0bc4abefe024480b4986854182001eb3
Ryan Di <git@aceougi.com>
2016-07-04 09:31:39 +0200

    Add custom dir

--- server.php
+++ server.php
@@ -3,11 +3,11 @@
 // Configuration
 $host = '127.0.0.1'; // 0.0.0.0 (for all)
 $port = 80; // 8080 (for an alternative port)
-$dir  = 'web/';
+$dir  = isset($argv[1]) ? '-t '.$argv[1] : '';
 $scr  = '"'.realpath(__DIR__.DIRECTORY_SEPARATOR.'server_router.php').'"';
 
 // Build
-$cmd = "php -S $host:$port -t $dir $scr";
+$cmd = "php -S $host:$port $dir $scr";
 
 // About
 echo "Listening on {$host}:{$port}".PHP_EOL;
