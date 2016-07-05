7d352e6e7eccdbef8d0c967984574452f16d1264
Ryan Di <git@aceougi.com>
2016-07-05 22:53:16 +0200

    Fix file exists and "?"

--- server_router.php
+++ server_router.php
@@ -1,6 +1,6 @@
 <?php
 
-if (file_exists($_SERVER['DOCUMENT_ROOT'].$_SERVER['REQUEST_URI']))
+if (file_exists($_SERVER['DOCUMENT_ROOT'].$_SERVER['SCRIPT_NAME']))
     return false;
 elseif (file_exists($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'index.php'))
     include $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'index.php';
